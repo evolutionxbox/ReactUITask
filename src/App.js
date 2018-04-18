55d2878ce664e3e039d61cdc1d03d95e18645ad5
Jonathan Cousins <evolutionxbox@gmail.com>
2018-04-18 16:11:16 +0100

    Example usage of initial tooltip component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
 import React from 'react'
+import Tooltip, { TooltipButton, TooltipArrow, TooltipLink } from './Tooltip'
+
+const CustomButton = ({onClick, children}) => <button onClick={onClick}>{children}</button>
 
 const App = () => (
-  <div />
+  <div>
+    <p>Normal</p>
+    <Tooltip>
+      <TooltipButton>Button CTA</TooltipButton>
+      <TooltipArrow />
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+    </Tooltip>
+    <p>Arrow positioned left</p>
+    <Tooltip>
+      <TooltipButton>Button CTA</TooltipButton>
+      <TooltipArrow position="left" />
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+    </Tooltip>
+    <p>200px width</p>
+    <Tooltip width={200}>
+      <TooltipButton>Button CTA</TooltipButton>
+      <TooltipArrow />
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+    </Tooltip>
+    <p>500px width</p>
+    <Tooltip width={500}>
+      <TooltipButton>Button CTA</TooltipButton>
+      <TooltipArrow />
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+    </Tooltip>
+    <p>Custom button text</p>
+    <Tooltip>
+      <TooltipButton>Custom button text</TooltipButton>
+      <TooltipArrow />
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+    </Tooltip>
+    <p>500px width with custom button component</p>
+    <Tooltip width={500}>
+      <TooltipButton View={CustomButton}>Recusandae ullam vitae magni</TooltipButton>
+      <TooltipArrow />
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+      <TooltipLink href='https://google.com'>Google</TooltipLink>
+    </Tooltip>
+  </div>
 )
 
 export default App
