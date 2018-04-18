f298add12380a84432d527d6024dd2bbd3599f29
Jonathan Cousins <evolutionxbox@gmail.com>
2018-04-18 17:19:31 +0100

    Update example usage of tooltip component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,88 @@
 import React from 'react'
-import Tooltip, { TooltipButton, TooltipArrow, TooltipLink } from './Tooltip'
-import Icon from './Icon'
+import Tooltip from './Tooltip'
 
-const CustomButton = ({onClick, children}) => <button onClick={onClick}>{children}</button>
+const items = [
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  }
+]
+
+const itemsWithIcons = [
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'favourites',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'giftcards',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'delivery',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'audioon',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'edit',
+      colour: 'white'
+    }
+  }
+]
 
 const App = () => (
   <div>
     <p>Normal</p>
-    <Tooltip>
-      <TooltipButton>Button CTA</TooltipButton>
-      <TooltipArrow />
-      <TooltipLink href='https://google.com'>
-        <Icon name="account" colour="white" height={20} width={20} />
-        Google
-      </TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-    </Tooltip>
+    <Tooltip buttonText='Button CTA' items={items} />
+    <p>Normal with icons</p>
+    <Tooltip buttonText='Button CTA' items={itemsWithIcons} />
     <p>Arrow positioned left</p>
-    <Tooltip>
-      <TooltipButton>Button CTA</TooltipButton>
-      <TooltipArrow position="left" />
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-    </Tooltip>
+    <Tooltip buttonText='Button CTA' arrow='left' items={items} />
     <p>200px width</p>
-    <Tooltip width={200}>
-      <TooltipButton>Button CTA</TooltipButton>
-      <TooltipArrow />
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-    </Tooltip>
+    <Tooltip width={200} buttonText='Button CTA' items={items} />
     <p>500px width</p>
-    <Tooltip width={500}>
-      <TooltipButton>Button CTA</TooltipButton>
-      <TooltipArrow />
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-    </Tooltip>
+    <Tooltip width={500} buttonText='Button CTA' items={items} />
     <p>Custom button text</p>
-    <Tooltip>
-      <TooltipButton>Custom button text</TooltipButton>
-      <TooltipArrow />
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-    </Tooltip>
-    <p>500px width with custom button component</p>
-    <Tooltip width={500}>
-      <TooltipButton View={CustomButton}>Recusandae ullam vitae magni</TooltipButton>
-      <TooltipArrow />
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-      <TooltipLink href='https://google.com'>Google</TooltipLink>
-    </Tooltip>
+    <Tooltip buttonText='Custom Button Text' items={items} />
+    <p>Always expanded</p>
+    <Tooltip buttonText='Button CTA' expanded={true} items={items} />
   </div>
 )
 
