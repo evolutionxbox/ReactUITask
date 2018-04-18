7235c76deb0de16a752f60a0ed79fca9ccf99873
Jonathan Cousins <evolutionxbox@gmail.com>
2018-04-18 17:12:44 +0100

    Move Icon component inside tooltip as to not rely on peer dependencies

--- src/Tooltip/Icon.js
+++ src/Tooltip/Icon.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import sprite from './icons/sprite.min.svg'
+import sprite from './sprite.min.svg'
 
 const Icon = ({
   name,
   colour,
-  width = 64,
-  height = 64,
+  width = 20,
+  height = 20,
 }) => {
-  if (colour !== 'black' && colour !== 'white') {
-    throw new Error('Warning: Icons must be black or white')
-  }
+  // if (colour !== 'black' && colour !== 'white') {
+  //   throw new Error('Warning: Icons must be black or white')
+  // }
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
+    <svg className='tooltip__icon' xmlns='http://www.w3.org/2000/svg' width={width} height={height}>
       <use xlinkHref={`${sprite}#${name}-${colour}`} />
     </svg>
   )
