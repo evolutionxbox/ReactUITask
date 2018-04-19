525e63004af806eb42e0c62c96c58493bf9ac351
Jonathan Cousins <evolutionxbox@gmail.com>
2018-04-19 23:54:44 +0100

    Move Icon component outside Tooltip

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,17 +1,18 @@
-/** @module Tooltip/Icon */
-
 import React from 'react'
 import PropTypes from 'prop-types'
+import { classNames } from '../utils'
 import sprite from './sprite.min.svg'
+import './icon.css'
 
 const Icon = ({
   name,
   colour,
   width,
   height,
+  className
 }) => {
   return (
-    <svg className='tooltip__icon' xmlns='http://www.w3.org/2000/svg' width={width} height={height}>
+    <svg className={classNames(['icon', className])} xmlns='http://www.w3.org/2000/svg' width={width} height={height}>
       <use xlinkHref={`${sprite}#${name}-${colour}`} />
     </svg>
   )
@@ -25,7 +26,7 @@ Icon.propTypes = {
   /**
    * Colour of the icon
    */
-  colour: PropTypes.string.isRequired,
+  colour: PropTypes.oneOf(['white', 'black']).isRequired,
   /**
    * Width of the icon
    */
@@ -33,14 +34,22 @@ Icon.propTypes = {
   /**
    * Height of the icon
    */
-  height: PropTypes.number
+  height: PropTypes.number,
+  /**
+   * Custom classname(s)
+   */
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string)
+  ]),
 }
 
 Icon.defaultProps = {
   name: 'home',
-  colour: 'white',
-  width: 20,
-  height: 20
+  colour: 'black',
+  width: 64,
+  height: 64,
+  className: ''
 }
 
 export default Icon
