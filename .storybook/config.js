7863cb819817c7c8b2ba204ed5e91f6a45ae6ce1
Jonathan Cousins <evolutionxbox@gmail.com>
2018-04-19 16:57:25 +0100

    Add storybook for react component documentation

---
+++ .storybook/config.js
@@ -0,0 +1,68 @@
+import { configure } from '@storybook/react'
+import { setDefaults } from '@storybook/addon-info'
+import { setOptions } from '@storybook/addon-options'
+
+// Options configure
+// Option defaults:
+setOptions({
+  /**
+   * name to display in the top left corner
+   * @type {String}
+   */
+  name: 'Yoox - Net-a-porter Group',
+  /**
+   * URL for name in top left corner to link to
+   * @type {String}
+   */
+  url: 'http://www.ynap.com/',
+  /**
+   * show addon panel as a vertical panel on the right
+   * @type {Boolean}
+   */
+  addonPanelInRight: true,
+})
+
+// Stories configure
+function loadStories() {
+  require('../src/stories')
+}
+configure(loadStories, module)
+
+// Info configure
+setDefaults({
+  /**
+   * Toggles display of header with component name and description
+   * @type {Boolean}
+   */
+  header: true,
+  /**
+   * Displays info inline vs click button to view
+   * @type {Boolean}
+   */
+  inline: true,
+  /**
+   * Displays the source of story Component
+   * @type {Boolean}
+   */
+  source: true,
+  /**
+   * Max props to display per line in source code
+   * @type {Number}
+   */
+  maxPropsIntoLine: 1,
+  /**
+   * Displays the first 10 characters of the prop name
+   * @type {Number}
+   */
+  maxPropObjectKeys: 10,
+  /**
+   * Displays the first 10 items in the default prop array
+   * @type {Number}
+   */
+  maxPropArrayLength: 10,
+  /**
+   * Displays the first 100 characters in the default prop string
+   * @type {Number}
+   */
+  maxPropStringLength: 100,
+})
---
+++ src/stories/index.js
@@ -0,0 +1,144 @@
+import React from 'react'
+
+// Storybook
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs, text, boolean, number, object, array } from '@storybook/addon-knobs/react'
+
+// Component related
+import Tooltip from '../Tooltip'
+import '../normalize.css'
+import '../index.css'
+
+// Data
+const items = [
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#'
+  }
+]
+const itemsWithIcons = [
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'favourites',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'giftcards',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'delivery',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'audioon',
+      colour: 'white'
+    }
+  },
+  {
+    text: 'Button Call To Action',
+    url: '#',
+    icon: {
+      name: 'edit',
+      colour: 'white'
+    }
+  }
+]
+
+const stories = storiesOf('Tooltip', module)
+stories.addDecorator(withKnobs)
+
+stories
+  .add('Normal',
+    withInfo(`
+      A simple Tooltip component which opens a list of links when the button is
+      clicked.
+
+      The links can be supplied through the \`items\` prop using an array of
+      \`item\` prop objects. Each \`item\` can be supplied with an icon.
+
+      In this example the items have no icons.
+    `)
+    (() =>
+      <Tooltip buttonText={text('ButtonText', 'Button CTA')} items={object('Items', items)} />
+    )
+  )
+  .add('Normal with icons',
+    withInfo(`
+      Same as "Normal" except supplied items have icons.
+    `)
+    (() =>
+      <Tooltip buttonText={text('ButtonText', 'Button CTA')} items={object('Items', itemsWithIcons)} />
+    )
+  )
+  .add('Arrow positioned left',
+    withInfo(`
+      Same as "Normal" except the Tooltip arrow is now left aligned.
+    `)
+    (() =>
+      <Tooltip buttonText={text('ButtonText', 'Button CTA')} arrowLeft={boolean('ArrowLeft', true)} items={object('Items', items)} />
+    )
+  )
+  .add('200px width',
+    withInfo(`
+      Same as "Normal" except the Tooltip is now 200px wide.
+    `)
+    (() =>
+      <Tooltip width={number('Width', 200)} buttonText={text('ButtonText', 'Button CTA')} items={object('Items', items)} />
+    )
+  )
+  .add('500px width',
+    withInfo(`
+      Same as "Normal" except the Tooltip is now 500px wide.
+    `)
+    (() =>
+      <Tooltip width={number('Width', 500)} buttonText={text('ButtonText', 'Button CTA')} items={object('Items', items)} />
+    )
+  )
+  .add('Custom button text',
+    withInfo(`
+      Same as "Normal" except the Tooltip button has custom text.
+    `)
+    (() =>
+      <Tooltip buttonText={text('ButtonText', 'Custom Button Text')} items={object('Items', items)} />
+    )
+  )
+  .add('Initially expanded',
+    withInfo(`
+      Same as "Normal" except the Tooltip links are expanded initially.
+    `)
+    (() =>
+      <Tooltip buttonText={text('ButtonText', 'Button CTA')} expanded={boolean('Expanded', true)} items={object('Items', items)} />
+    )
+  )
