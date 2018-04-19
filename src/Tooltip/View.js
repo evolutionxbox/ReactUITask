643cf56bf9d4d2c6e0e237f8592c510861245d5c
Jonathan Cousins <evolutionxbox@gmail.com>
2018-04-19 17:01:01 +0100

    Removed duplicate default prop types declaration (already covered in index component)

--- src/Tooltip/View.js
+++ src/Tooltip/View.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Item from './Item'
 import './tooltip.css'
 
 const View = ({
-  buttonText,
-  arrow,
-  items,
-  width,
-  expanded,
-  onClick
+  buttonText = '',
+  arrowLeft = false,
+  items = [],
+  width = 0,
+  expanded = false,
+  onClick = () => {}
 }) => {
   // style and classes
   const expandedClass = expanded ? 'tooltip--expanded' : ''
@@ -31,24 +30,6 @@ const View = ({
       </div>
     </div>
   )
-}
-
-View.propTypes = {
-  buttonText: PropTypes.string.isRequired,
-  arrow: PropTypes.string,
-  items: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    url: PropTypes.PropTypes.string.isRequired,
-    icon: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      colour: PropTypes.string.isRequired,
-      width: PropTypes.number,
-      height: PropTypes.number
-    })
-  })).isRequired,
-  width: PropTypes.number,
-  expanded: PropTypes.bool,
-  onClick: PropTypes.func.isRequired
 }
 
 export default View
